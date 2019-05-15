9a1eb13968d74179527e2fa98f8b5bc82ae6d40e
Liya Roy <liya.roy@rubyians.com>
2019-05-15 23:16:29 +0530

    Adding more fields to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product
   include Mongoid::Document
   field :name, type: String
   field :price, type: BigDecimal
+  field :manufactured_by, type: String
+  field :released_on, type: Date
 end

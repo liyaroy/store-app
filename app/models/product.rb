7f55fb2e54ebf88443482b5abd42258d3bb3bb73
Liya Roy <liya.roy@rubyians.com>
2019-06-12 22:34:02 +0530

    Add new model review

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :name, type: String
   field :price, type: BigDecimal
   field :manufactured_by, type: String
-  field :released_on, type: Date
+  field :released_on, type: String
+
+  embeds_many :reviews
 end
---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :content, type: String
+
+  embedded_in :product
+end
